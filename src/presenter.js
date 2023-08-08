bc423218619b1426be89819b0f51a25fac53b310
Zatt010 <afuheho1@gmail.com>
2023-08-08 14:21:21 -0400

    cambios para multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,5 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
@@ -11,5 +12,19 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>Suma: " + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+const primerNumeroMultiplicar = document.querySelector("#primer-numero-multiplicar");
+const segundoNumeroMultiplicar = document.querySelector("#segundo-numero-multiplicar");
+const formMultiplicar = document.querySelector("#multiplicar-form");
+const divMultiplicar = document.querySelector("#resultado-multiplicar-div");
+
+formMultiplicar.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const primerNumero = Number.parseInt(primerNumeroMultiplicar.value);
+  const segundoNumero = Number.parseInt(segundoNumeroMultiplicar.value);
+
+  divMultiplicar.innerHTML = "<p>Multiplicación: " + multiplicar(primerNumero, segundoNumero) + "</p>";
 });
